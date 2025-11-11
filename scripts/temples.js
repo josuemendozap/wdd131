ce8aa92efeec0e9d88a90fca85140e209e591b79
josuemendozap <josue.mendozap@gmail.com>
2025-11-11 15:44:16 -0600

    last changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,12 @@ const today = new Date();
 year.innerHTML = `<span>${today.getFullYear()}</span>`;
 
 document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
+
+
+const mainnav = document.querySelector('nav');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
